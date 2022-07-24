c8bcb37106fc8fd182c368bcde4f651ae30b22e5
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-07-24 19:36:01 +0400

    menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,35 @@
 const myMobile = {
-  Android: function() {
-      return navigator.userAgent.match(/Android/i);
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
   },
-  BlackBerry: function() {
-      return navigator.userAgent.match(/BlackBerry/i);
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
   },
-  iOS: function() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
   },
-  Opera: function() {
-      return navigator.userAgent.match(/Opera Mini/i);
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
   },
-  Windows: function() {
-      return navigator.userAgent.match(/IEMobile/i);
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
   },
-  any: function() {
-      return (myMobile.Android() || 
-              myMobile.BlackBerry() || 
-              myMobile.iOS() || 
-              myMobile.Opera() || 
-              myMobile.Windows());
-  }
+  any: function () {
+    return myMobile.Android() || myMobile.BlackBerry() || myMobile.iOS() || myMobile.Opera() || myMobile.Windows();
+  },
 };
 
-if( myMobile.any() ) {
-document.body.classList.add('_touch');
-let menuArrows = document.querySelectorAll('.menu__arrow');
+if (myMobile.any()) {
+  document.body.classList.add('_touch');
+  let menuArrows = document.querySelectorAll('.menu__arrow');
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener('click', function(){
+        menuArrow.parentElement.classList.toggle('_active');
+      })
+    }
+  }
 } else {
   document.body.classList.add('_pc');
 }
